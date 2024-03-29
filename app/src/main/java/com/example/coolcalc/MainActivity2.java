78805c3550bf784eba13d16a75929aba3590efbe
shanag1801 <shanag0110@gmail.com>
2024-03-29 19:02:38 +0300

    added passing data between activities functionality.

--- app/src/main/java/com/example/coolcalc/MainActivity2.java
+++ app/src/main/java/com/example/coolcalc/MainActivity2.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView tv;
+    Button btn;
+    int lastRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         tv = (TextView) findViewById(R.id.tv);
-        Intent gi = getIntent();
-        int lastRes = gi.getIntExtra("lastRes", 0);
-        tv.setText(lastRes);
+        btn = (Button) findViewById(R.id.btn);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null)
+            lastRes = extras.getInt("lastRes");
+        tv.setText(String.valueOf(lastRes));
     }
 
     public void back(View view) {
